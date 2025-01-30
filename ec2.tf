5033a9f88b107f681e4de1d1404e1408d5de2120
sandeepanainala <nainala_sandeep@yahoo.com>
2025-01-30 21:20:01 +0530

    Terraform provisioners local

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "db" {
-    ami = "ami-0c55b159cbfafe1f0"
+    ami = "ami-0b4f379183e5706b9"
     instance_type = "t2.micro"
     vpc_security_group_ids = ["sg-006bc654442a8a0cc"]
 
