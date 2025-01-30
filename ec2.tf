1bbb51c11fe108e19ed24573803bb4aee0b45078
sandeepanainala <nainala_sandeep@yahoo.com>
2025-01-30 21:26:22 +0530

    Terraform provisioners local

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,14 @@
 resource "aws_instance" "db" {
-    ami = "ami-0b4f379183e5706b9"
-    instance_type = "t2.micro"
+    ami                    = "ami-0b4f379183e5706b9"
+    instance_type          = "t2.micro"
     vpc_security_group_ids = ["sg-006bc654442a8a0cc"]
 
     provisioner "local-exec" {
-      command = "echo ${aws_instance.db.public_ip} >> private_ips.txt"  # This will write the private IP of the instance to a file
+        command = "echo ${aws_instance.db.public_ip} >> private_ips.txt"
+        # This will write the private IP of the instance to a file
+    }
+    provisioner "local-exec" {
+        command = "ansible-playbook -i private_ips.txt web.yml"
+        # This will run the ansible playbook on the private IP of the instance
     }
 }
